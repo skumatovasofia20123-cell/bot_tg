e7eb74a21603087d2c90e698e16095ac52c3866d
София(Sofia) <skumatovasofia20123@gmail.com>
2025-12-25 19:24:10 +0300

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,20 +1,44 @@
+import telebot
+import random
+from bot_new import gen_pass
+from telebot.types import ReactionTypeEmoji
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7988454403:AAE7GSmXKp499U5v0NUFQ0k-abQl5PV-W_I")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+        bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
+        bot.reply_to(message, "Привет! Как дела?")
     
 @bot.message_handler(commands=['password'])
 def passwords(message):
-    passwordss = gen_pass(10)
-    bot.reply_to(message,passwordss)
-    
+        passwordss = gen_pass(10)
+        bot.reply_to(message,passwordss)
 
 @bot.message_handler(commands=['smile'])
 def smile(message):
     elements = "😀😃😄😁😆😅🤣😂🙂🙃🫠😉😊😇🥰😍🤩😘😗☺️😚😙🥲😋😛😜🤪😝🤑🤗🤭🫢🫣🤫🤔🫡🤐🤨😐😑😶🫥😶‍🌫️😏😒🙄😬😮‍💨🤥🫨🙂‍↔️🙂‍↕️😌😔😪🤤😴☹️😮😯😲😳🥺🥹😦😧😨😰😥😢😭😱😖😣😞😓😩😫🥱😤😡😠🤬😈👿💀☠️💩🤡👹👺👻👽👾🤖😺😸😹😻😼😽🙀😿😾🙈🙉🙊💋💯💢💥💫💦💨🕳️💤👋🤚🖐️✋🖖🫱🫲🫳🫴🫷🫸👌🤌🤏✌️🤞🫰🤟🤘🤙👈👉👆🖕👇☝️🫵👍👎✊👊🤛🤜👏🙌🫶👐🤲🤝🙏✍️💅🤳💪🦾🦿🦵🦶👂🦻👃🧠🫀🫁🦷🦴👀👁️👅👄🫦💌💘💝💖💗💓💞💕💟❣️💔❤️‍🔥❤️‍🩹❤️🩷🧡💛💚💙🩵💜🤎🖤🩶🤍"
     bot.reply_to(message, random.choice(elements))
+    
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#         bot.reply_to(message, message.text)
 
+
+# Send a reactions to all messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
     
 bot.polling()
